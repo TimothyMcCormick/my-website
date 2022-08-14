6ea2c976db5a2e218f9e03764f77b45d8fe106c6
Timothy McCormick <breaking490@gmail.com>
2022-08-14 13:40:46 -0600

    init still needs contact and polishing

---
+++ app.js
@@ -0,0 +1,9 @@
+function openTower() {
+  window.open("https://towerevents.herokuapp.com/#/")
+}
+function openOnTracker() {
+  window.open("https://ontracker-capstone.herokuapp.com/#/")
+}
+function openKeepr() {
+  window.open("https://keepr-89.herokuapp.com/#/")
+}
